3b9178b210c2a4199c5dc8a0daaece87e4cfc346
99Kies <1290017556@qq.com>
2020-03-23 20:08:17 +0800

    update Repeter.py

--- Get_Requests_From_Local_NIC/Repeter.py
+++ Get_Requests_From_Local_NIC/Repeter.py
@@ -1,6 +1,10 @@
 #coding:utf-8
 import sys
+import requests
+import time
+from scapy.utils import PcapReader
 ##resend the package in package.txt, and get the return from server.
+host=''
 fd=open(sys.path[0]+'//package.txt')
 def getmidstring(html, start_str, end):
     start = html.find(start_str)
@@ -8,16 +12,50 @@ def getmidstring(html, start_str, end):
         start += len(start_str)
         end = html.find(end, start)
         if end >= 0:
-            return html[start:end].strip()
+            return str(html[start:end].strip())
 def Read_package():
     line=fd.readline()
     while ('GET' not in line) and ('POST' not in line):
         line=fd.readline()
-        #print(line)
     if 'GET' in line:
         msd='GET'
-        print(getmidstring(line,'GET ',' HTTP/'))
+        host=getmidstring(line,'GET ',' HTTP/')
+        requests_get(line, host)
+    #else:
+    #    msd='POST'
+    #    host=getmidstring(line,'POST ',' HTTP/')
 
-Read_package()
-Read_package()
-Read_package()
+
+def requests_get(line, host):
+    headers={}
+    headers.clear()
+    # time.sleep(1)
+    while('Host:' not in line):
+        line = fd.readline()
+        head = line.split(": ")
+        if "\n" not in head[0] and head[0] != '':
+            headers[head[0]]=head[1][:-1]
+    # print(headers)
+    host='http://'+str(getmidstring(line,'Host:','\n'))+str(host)
+    print(host)
+    resp=requests.get(url=host,headers=headers).text
+    print(resp)
+
+
+def requests_post(line, host):
+    headers={}
+    headers.clear()
+    # time.sleep(1)
+    while('Host:' not in line):
+        line = fd.readline()
+        head = line.split(": ")
+        if "\n" not in head[0] and head[0] != '':
+            headers[head[0]]=head[1][:-1]
+
+    host='http://'+str(getmidstring(line,'Host:','\n'))+str(host)
+    print(host)
+    resp=requests.post(url=host,headers=headers).text
+    print(resp)
+
+while(1):
+    Read_package()
