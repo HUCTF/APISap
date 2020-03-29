40a330b859d27c681ee7015f5af875630778eb2d
yx <103040077@qq.com>
2020-03-29 23:15:27 +0800

    update sql_operations.py

--- api_server/sql_operation.py
+++ api_server/sql_operation.py
@@ -4,31 +4,44 @@ from flask_sqlalchemy import SQLAlchemy
 import pymysql
 from sqlalchemy.orm import mapper
 from sqlalchemy import Table,MetaData
-metadata = MetaData()
-app=Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://123456:123456@localhost:3306/token1'  # 这里登陆的是root用户，要填上自己的密码，MySQL的默认端口是3306，填上之前创建的数据库名text1
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True  # 设置这一项是每次请求结束后都会自动提交数据库中的变动
-
-db = SQLAlchemy(app)  # 实例化
-tablename=''
 import time
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+Base = declarative_base()
+app=Flask(__name__)
+engine = create_engine('mysql://123456:123456@localhost:3306/token1?charset=utf8', echo=True)
+
+Session = sessionmaker(bind=engine)
+
+session = Session()
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://123456:123456@localhost:3306/token1'  # 这里登陆的是root用户，要填上自己的密码，MySQL的默认端口是3306，填上之前创建的数据库名text1
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True  # 设置这一项是每次请求结束后都会自动提交数据库中的变动
+#
+# db = SQLAlchemy(app)  # 实例化
+tablename=''
 
 
-class token1(db.Model):
+class token(Base):
     __tablename__ = 'token_1'
-    user_id = db.Column(db.String(255), unique=True,primary_key=True,nullable=False)
-    time_code = db.Column(db.String(255), nullable=False)
-    token = db.Column(db.String(255), nullable=False)
+    __table_args__ = {
+        'mysql_engine': 'InnoDB',
+        'mysql_charset': 'utf8'
+    }
+    user_id = Column(String(255), unique=True,primary_key=True,nullable=False)
+    time_code = Column(String(255), nullable=False)
+    token = Column(String(500), nullable=False)
 
     # @orm.reconstructor
     # def __init__(self):
     #     self.api_name = ["id", "nickname", "mobile", "email"]
     def init(self,url):
-        # id = db.Column(db.Integer, primary_key=True,unique=True,autoincrement=True
+        # id = Column(Integer, primary_key=True,unique=True,autoincrement=True
         # try:
-        # items = db.session.execute("select table_nm from server WHERE url=%s "%(url))
-        results = server.query.filter_by(url=url).all()
+        # items = session.execute("select table_nm from server WHERE url=%s "%(url))
+        results = session.query(server).filter_by(url=url).all()
         # print(results)
         resData = []
         for x in results:
@@ -38,9 +51,10 @@ class token1(db.Model):
         # print(resData)
         if resData!=[]:
             # print(items[0][0])
-            __tablename__ = resData[0]['table_nm']
-            tablename=self.__tablename__
-            print(self.__tablename__)
+            self.__table__.name = resData[0]['table_nm']
+            # __init__(app)
+            tablename=self.__table__.name
+            # print(tablename)
             print('找表成功')
             return tablename
             # return {'code':200,'msg':'找表成功'}
@@ -51,76 +65,76 @@ class token1(db.Model):
         #     print('地址格式错误')
         #     return {'code': 10001, 'msg': '地址格式错误'}
         # self.__tablename__=
+
 #server表
-class server(db.Model):
+class server(Base):
     __tablename__ = 'server'
-    # id = db.Column(db.Integer, primary_key=True,unique=True,autoincrement=True)
-    url = db.Column(db.String(255), unique=True,primary_key=True,nullable=False)
-    pub_key = db.Column(db.String(255), nullable=False)
-    private_key = db.Column(db.String(255), nullable=False)
-    table_nm= db.Column(db.String(255), nullable=False)
+    __table_args__ = {
+        'mysql_engine': 'InnoDB',
+        'mysql_charset': 'utf8'
+    }
+    # id = Column(Integer, primary_key=True,unique=True,autoincrement=True)
+    url = Column(String(255), unique=True,primary_key=True,nullable=False)
+    pub_key = Column(String(255), nullable=False)
+    private_key = Column(String(255), nullable=False)
+    table_nm= Column(String(255), nullable=False)
 
 
-token=token1()
 #token表的操作
 class db_operation:
     def __init__(self):
-        self.token=''
-    # 增
-    # token = token1()
-    # token.init('123')
-    def __init__(self):
-        self.url=''
+        token=''
     def init(self,url):
-        # print(url)
-        token1.init(token1,url)
-        self.token=token1()
-        print(self.token.__tablename__)
-        print("--------------")
+        token.init(token,url)
+
+        # print(self.token.__tablename__)
+        # print("--------------")
         # token=token1()
         # print(url)
-
-    def insert(self,user_id,time_code,token):
-        db.session.add(token(user_id=user_id,time_code=time_code,token=token))
-        db.session.commit()
+    def insert(self,user_id1,time_code1,token1):
+        # print(user_id1)
+        # print(time_code1)
+        # print(token1)
+        session.add(token(user_id=user_id1,time_code=time_code1,token=token1))
+        session.commit()
         print('单个数据添加成功')
 
     def createMany(self,list):
         # users=[Role(name="lisi"),Role(name="wangwu"),Role(name="zhaosi")]
-        db.session.add_all(list)
-        db.session.commit()
+        session.add_all(list)
+        session.commit()
         print('多个数据添加成功')
 
     # 删
     def deleteis(self,user_id):
-        results = self.token.query.filter_by(user_id=user_id).all()
-        db.session.delete(results)
-        db.session.commit()
+        results = session.query(token).filter_by(user_id=user_id).all()
+        session.delete(results)
+        session.commit()
         print('数据删除成功')
 
     #删除已经过时的密钥
     def delete_task(self):
         now=self.get_time()/1000000-86400
-        items=db.session.execute("delete  from token  where (time_code+0)/1000000<'%d'"%(now))
+        items=session.execute("delete  from token  where (time_code+0)/1000000<'%d'"%(now))
         items=list(items)
         print(items)
-        # db.session.execute("delete from user where id=1 ")
+        # session.execute("delete from user where id=1 ")
 
 
     # # 改
     def update(self,uer_id,time_code,token):
-        results = token.query.filter_by(user_id=uer_id).all()
+        results = session.query(token).filter_by(user_id=uer_id).all()
         # print(results[0].title)
         results[0].time_code = time_code
         results[0].token=token
-        db.session.commit()
+        session.commit()
         print('修改成功')
 
 
     #
     # # 查
     def search_by_user_id(self,user_id):
-        results = self.token.query.filter_by(user_id=user_id).all()
+        results = session.query(token).filter_by(user_id=user_id).all()
         # print(results)
         resData = []
         for x in results:
@@ -136,7 +150,7 @@ class db_operation:
             return resData
 
     def search_by_time_code(self,time_code):
-        results = token.query.filter_by(time_code=time_code).all()
+        results = session.query(token).filter_by(time_code=time_code).all()
         # print(results)
         resData = []
         for x in results:
@@ -153,7 +167,7 @@ class db_operation:
 
 
     def search_by_token(self,token):
-        results = self.token.query.filter_by(token=token).all()
+        results = session.query(token).filter_by(token=token).all()
         # print(results)
         resData = []
         for x in results:
@@ -170,19 +184,20 @@ class db_operation:
 
     #获取所有数据
     def search_all(self):
-        dict = token.query.all()
+        dict = session.query(token).all()
         for each in dict:
             # print(each)
             print(each.user_id + '|' + each.time_code + '|' + each.token)
         return dict
 
     def checkhave(self,user_id):
-        print(self.token.__tablename__)
-        results = self.token.query.filter_by(user_id=user_id).all()
-        print(results[0].token)
-        print(12345)
+        print(token.__tablename__)
+        results = session.query(token).filter_by(user_id=user_id).all()
+        # print(results)
+        # print(results[0].token)
+        # print(12345)
         if results ==[]:
-            print(0)
+            # print(0)
             return 0
         else:
             print(1)
@@ -203,46 +218,46 @@ class server_operation:
     def __init__(self):
         self.url = ''
     def insert(self, url, pub_key, private_key,table_nm):
-        # print(url)
-        db.session.add(server(url=url,pub_key=pub_key, private_key=private_key,table_nm=table_nm))
-        db.session.commit()
+
+        session.add(server(url=url,pub_key=pub_key, private_key=private_key,table_nm=table_nm))
+        session.commit()
         print('单个数据添加成功')
 
     def createMany(self, list):
         # users=[Role(name="lisi"),Role(name="wangwu"),Role(name="zhaosi")]
-        db.session.add_all(list)
-        db.session.commit()
+        session.add_all(list)
+        session.commit()
         print('多个数据添加成功')
 
     # 删
     def deleteis(self, url):
-        results = server.query.filter_by(url=url).all()
-        db.session.delete(results)
-        db.session.commit()
+        results = session.query(server).filter_by(url=url).all()
+        session.delete(results)
+        session.commit()
         print('数据删除成功')
 
     # 删除已经过时的密钥
     # def delete_task(self):
     #     now = self.get_time() / 1000000 - 86400
-    #     items = db.session.execute("delete  from server  where (time_code+0)/1000000<'%d'" % (now))
+    #     items = session.execute("delete  from server  where (time_code+0)/1000000<'%d'" % (now))
     #     items = list(items)
     #     print(items)
-        # db.session.execute("delete from user where id=1 ")
+        # session.execute("delete from user where id=1 ")
 
     # # 改
     def update(self,  url, pub_key, private_key,table_nm):
-        results = server.query.filter_by(url=url).all()
+        results = session.query(server).filter_by(url=url).all()
         # print(results[0].title)
         results[0].pub_key = pub_key
         results[0].private_key = private_key
         results[0].table_nm=table_nm
-        db.session.commit()
+        session.commit()
         print('修改成功')
 
     #
     # # 查
     def search_by_url(self,  url):
-        results = server.query.filter_by(url=url).all()
+        results = session.query(server).filter_by(url=url).all()
         # print(results)
         resData = []
         for x in results:
@@ -259,7 +274,7 @@ class server_operation:
             return resData
 
     def search_by_table_nm(self, table_nm):
-        results = server.query.filter_by(table_nm=table_nm).all()
+        results = session.query(server).filter_by(table_nm=table_nm).all()
         # print(results)
         resData = []
         for x in results:
@@ -277,7 +292,7 @@ class server_operation:
 
     # 获取所有数据
     def search_all(self):
-        dict = server.query.all()
+        dict = session.query(server).all()
         # print('good')
         for each in dict:
             # print(each)
@@ -287,7 +302,7 @@ class server_operation:
     def checkhave(self, url):
         # print('goddddddd' + url)
         # url='123'
-        results = server.query.filter_by(url=url).all()
+        results = session.query(server).filter_by(url=url).all()
         # print('adas')
         if results == []:
             return 0
@@ -301,7 +316,7 @@ class server_operation:
             "`token` varchar(255) CHARACTER SET utf8 NOT NULL,"+\
             "PRIMARY KEY (`user_id`) USING BTREE)"+\
             " ENGINE=InnoDB DEFAULT CHARSET=latin1;"
-        db.session.execute(str)
+        session.execute(str)
         print('建表成功')
 
 if __name__ == "__main__":
@@ -309,7 +324,8 @@ if __name__ == "__main__":
     # print(resData)
     op = db_operation()
     op.init('127.0.0.1:8886')
-
+    # op = db_operation()
+    op.checkhave('2019')
     # op.search_all()
     # sv = server_operation()
     # sv.search_by_url('127.0.0.1:8886')
