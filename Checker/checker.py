b062a2ff6c2ac5207b1aadd7bfd1dea700b7ab88
Luz <58980928+Luz233@users.noreply.github.com>
2020-03-30 23:31:16 +0800

    信息提取

--- Checker/checker.py
+++ Checker/checker.py
@@ -1,10 +1,15 @@
 import string
 import re
-#def id_code(resp):
+def id_code(resp):
+    pat="[1-9]\d{5}(?:19|20)\d\d(?:0[1-9]|1[012])(?:0[1-9]|[12]\d|3[01])\d{4}"
+    res= re.findall(pat,resp)
+    print(res)
 def phone_number(resp):
-    pat=r"(1(([35789]\d)|(47))\d{8})"
+    pat="(1(([35789]\d)|(47))\d{8})"
     res= re.findall(pat,resp)
     print(res)
 #def home_address(resp):
-#def checker(resp):
-phone_number("dfad14735354568fewdf13135354568klmdfa17816783987")
+def checker(resp):
+    phone_number(resp)
+    id_code(resp)
+checker("330424199901011122233aaaaaa hyluz17816783987")
