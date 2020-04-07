09c01ee8427f6bc152b081648142af7aff54ad2a
99Kies <1290017556@qq.com>
2020-04-07 20:40:41 +0800

    Add Class

--- Get_Requests_From_Local_NIC/NIC_package_get.py
+++ Get_Requests_From_Local_NIC/NIC_package_get.py
@@ -4,77 +4,102 @@ import sys
 import os
 from datetime import datetime
 from threading import Timer
-path=''
-filename=''
-filename_txt=''
-filename_pcap=''
-package_output='all' #'screen'/'document'/'all'/'pcap'
-catch_method=0 #0连续抓包 1按数量抓包
-Time_conversion=5 #使用连续抓包时的数据包保存时间间隔，仅在catch_method=0时有效
-package_num=1000  #使用按数量抓包时的数据包抓取数量，仅在catch_method=1时有效
-iface='Realtek 8821CE Wireless LAN 802.11ac PCI-E NIC'
-pcap=[]
-def create_dir_and_file():
-    isExists=os.path.exists(path)
-    if not isExists:
-        os.makedirs(path)
-    fileisExists=os.path.exists(filename_txt)
-    if not fileisExists:
-        #os.mknod(filename_txt)
-        #os.mknod(filename_pcap)
-        fp = open(filename_txt,'w')
+
+class NICPackage:
+
+    def __init__(self, IFACE=None, NEEDPCAP=1000):
+        # self.path=''
+        # self.filename=''
+        # self.filename_txt=''
+        # self.filename_pcap=''
+
+        WIN = sys.platform.startswith('win')
+        if WIN:
+            self.prefix = '\\'
+        else:
+            self.prefix = '/'
+        self.package_output='all' #'screen'/'document'/'all'/'pcap'
+        self.catch_method=0 #0连续抓包 1按数量抓包
+        self.Time_conversion=5 #使用连续抓包时的数据包保存时间间隔，仅在catch_method=0时有效
+        self.package_num=NEEDPCAP  #使用按数量抓包时的数据包抓取数量，仅在catch_method=1时有效
+        # self.iface='Realtek PCIe GbE Family Controller'
+        self.iface=IFACE
+        self.pcap=[]
+        self.path = sys.path[0] + self.prefix + str(datetime.now().strftime("%Y-%m-%d_%H"))
+        self.filename = self.path + self.prefix + str(datetime.now().strftime("%Y-%m-%d_%H"))
+        self.filename_pcap = self.filename + '.pcap'
+        self.filename_txt = self.filename + '.txt'
+        
+    def create_dir_and_file(self):
+        isExists=os.path.exists(self.path)
+        if not isExists:
+            os.makedirs(self.path)
+        fileisExists=os.path.exists(self.filename_txt)
+        if not fileisExists:
+            #os.mknod(filename_txt)
+            #os.mknod(filename_pcap)
+            fp = open(self.filename_txt,'w')
+            fp.close()
+            wrpcap(self.filename_pcap,'')
+
+        self.pcap = rdpcap(self.filename_pcap)
+    
+    def package_print(self, packet):
+        if (self.package_output == 'screen') or (self.package_output == 'all'):
+            print("\n".join(packet.sprintf("{Raw:%Raw.load%}").split(r"\r\n"))+'\n\n\n')
+        if (self.package_output == 'document') or (self.package_output == 'all'):
+            with open(self.filename_txt,'a') as outputxt:
+                outputxt.write("\n".join(packet.sprintf("{Raw:%Raw.load%}").split(r"\r\n"))+'\n\n\n')
+                # outputxt.close()
+        if (self.package_output == 'pcap'):
+            pcap.extend(packet)
+            wrpcap(self.filename_pcap,pcap)
+            print(self.filename_pcap)
+            #print("\n".join(packet.sprintf("{Raw:%Raw.load%}").split(r"\r\n"))+'\n\n\n')
+        #return "\n".join(packet.sprintf("{Raw:%Raw.load%}").split(r"\r\n"))+'\n\n\n'
+    def time(self):
+        
+        self.filename = self.path + self.prefix + str(datetime.now().strftime("%Y-%m-%d_%H"))
+        self.filename_pcap = self.filename + '.pcap'
+        self.filename_txt = self.filename + '.txt'
+        wrpcap(self.filename_pcap,'')
+        fp = open(self.filename_txt,'w')
         fp.close()
-        wrpcap(filename_pcap,'')
+        #create_dir_and_file()
+        #print(filename_pcap)
+        Timer(self.Time_conversion, self.time).start()
+    
+    def mainrun(self):
+        self.create_dir_and_file()
 
-    pcap=rdpcap(filename_pcap)
-def package_print(packet):
-    if (package_output == 'screen') or (package_output == 'all'):
-        print("\n".join(packet.sprintf("{Raw:%Raw.load%}").split(r"\r\n"))+'\n\n\n')
-    if (package_output == 'document') or (package_output == 'all'):
-        outputxt=open(filename_txt,'a')
-        print(filename)
-        outputxt.write("\n".join(packet.sprintf("{Raw:%Raw.load%}").split(r"\r\n"))+'\n\n\n')
-    if (package_output == 'pcap'):
-        pcap.extend(packet)
-        wrpcap(filename_pcap,pcap)
-        print(filename_pcap)
-        #print("\n".join(packet.sprintf("{Raw:%Raw.load%}").split(r"\r\n"))+'\n\n\n')
-    #return "\n".join(packet.sprintf("{Raw:%Raw.load%}").split(r"\r\n"))+'\n\n\n'
-def time(): 
-    global filename
-    global filename_pcap
-    global filename_txt
-    filename=path+'//'+str(datetime.now().strftime("%Y-%m-%d %H:%M:%S").replace(':','_'))
-    filename_pcap=filename+'.pcap'
-    filename_txt=filename+'.txt'
-    wrpcap(filename_pcap,'')
-    fp = open(filename_txt,'w')
-    fp.close()
-    #create_dir_and_file()
-    #print(filename_pcap)
-    Timer(Time_conversion, time).start()
-path=sys.path[0]+'//'+str(datetime.now().strftime("%Y-%m-%d %H:%M:%S").replace(':','_'))
-filename=path+'//'+str(datetime.now().strftime("%Y-%m-%d %H:%M:%S").replace(':','_'))
-filename_pcap=filename+'.pcap'
-filename_txt=filename+'.txt'
-create_dir_and_file()
+        if self.catch_method==0:
+            t = Timer(self.Time_conversion, self.time) 
+            t.start()
+            while(1):
+                sniff(
+                iface=self.iface,
+                count=1,
+                prn=self.package_print,
+                lfilter=lambda p: ("GET" in str(p)) or ("POST" in str(p)),
+                filter="tcp")
+        else:
+            sniff(
+            iface=self.iface,
+            count=self.package_num,
+            prn=self.package_print,
+            lfilter=lambda p: ("GET" in str(p)) or ("POST" in str(p)),
+            filter="tcp")
 
-if catch_method==0:
-    t = Timer(Time_conversion,time) 
-    t.start()
-    while(1):
-        sniff(
-        iface=iface,
-        count=1,
-        prn=package_print,
-        lfilter=lambda p: ("GET" in str(p)) or ("POST" in str(p)),
-        filter="tcp")
-else:
-    sniff(
-    iface=iface,
-    count=package_num,
-    prn=package_print,
-    lfilter=lambda p: ("GET" in str(p)) or ("POST" in str(p)),
-    filter="tcp")
+            #iface='XXX'  监听本地名为XXX的网卡
 
-    #iface='XXX'  监听本地名为XXX的网卡
+
+def NICRUN(netname=None, needpcap=1000):
+    # a = NICPackage('Realtek PCIe GbE Family Controller')
+    if netname:
+        print("Start get NIC package...")
+        a = NICPackage(IFACE=netname, NEEDPCAP=needpcap)
+        a.mainrun()
+    else:
+        print("=======================================================")
+        print('请设置网卡名！！！')
+        print("=======================================================")
