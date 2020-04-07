efbdb4fa43cde2ed5569b30b9aaef85a3574018d
99Kies <1290017556@qq.com>
2020-04-07 20:41:13 +0800

    Update app.py

--- ScanServer/app.py
+++ ScanServer/app.py
@@ -2,13 +2,12 @@
 from flask import Flask, render_template_string
 from flask import render_template, request, flash, redirect, url_for , session
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField, IntegerField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, SubmitField, SelectField, IntegerField, TextAreaField
+from wtforms.validators import DataRequired, Length, Required
 from flask_bootstrap import Bootstrap
-import base64
-import psutil
 
-from scanapi.v1.NIC_package_get import package_print
+from threading import Thread
+import psutil
 
 def get_net():
     '''
@@ -22,6 +21,9 @@ def get_net():
             if item[0] == 2 and not item[1][:3] != '192':
                 netcard_info.append((k, item[1]))
     return netcard_info
+
+
+
 
 app = Flask(__name__)
 app.config[ "SECRET_KEY"] =  "s_e_c_r_e_t_k_e_y_h_u_c_t_f"
@@ -38,6 +40,11 @@ class ScipyForm(FlaskForm):
     spider = SubmitField("开启扫描")
     repeter = SubmitField("重发数据包")
 
+class PrintLogForm(FlaskForm):
+    scipylog = TextAreaField("数据包抓取记录",validators=[Required()])
+    spiderlog = TextAreaField("爬虫记录",validators=[Required()])
+    repeterlog = TextAreaField("重发器记录",validators=[Required()])
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     form = NameForm()
@@ -46,34 +53,49 @@ def index():
     form.netname.choices = net
     if form.validate_on_submit():
         netname = form.netname.data
-        needpacp = form.needpcap.data
-        session['you_can_take_it_']=netname
-        return redirect(url_for('pacp', netname=netname, needpacp=needpacp))
+        needpcap = form.needpcap.data
+        session['netname'] = netname
+        session['needpcap'] = needpcap
+        return redirect(url_for('pcap', netname=netname, needpcap=needpcap))
     return render_template('index.html', form=form)
 
-@app.route("/pacp", methods=['GET', 'POST'])
-def pacp():
+@app.route("/pcap", methods=['GET', 'POST'])
+def pcap():
     form = NameForm()
+    form1 = PrintLogForm()
     netname = request.args.get("netname", None)
-    needpacp = request.args.get("needpacp", None)
-    if session.get('you_can_take_it_'):
+    needpcap = request.args.get("needpcap", None)
+    stop = 1
+    if session.get('netname') and session.get('needpcap'):
         form = ScipyForm()
+        netname = session.get('netname')
+        needpcap = session.get('needpcap')
         if form.validate_on_submit():
             if form.scipy.data:
-                sniff(
-                    iface=netname,
-                    prn=package_print,
-                    lfilter=lambda p: ("GET" in str(p)) or ("POST" in str(p)),
-                    filter="tcp")
-                    #iface='XXX'  监听本地名为XXX的网卡
+                from scanapi.v2.NIC_package_get import NICRUN
+                stop = 0
                 flash('开始抓包！')
                 
+                thread = Thread(target=NICRUN, args=[netname, needpcap])
+                # 使用多线程
+                thread.start()
+
             elif form.spider.data:
+                from scanapi.v2.scanapi import RQRUN
+                stop = 0
                 flash('开始爬虫！')
+                thread = Thread(target=RQRUN)
+                
+                # 使用多线程
+                thread.start()
+
             elif form.repeter.data:
+                thread = Thread(target=send_async_email, args=[app, message])
+                # 使用多线程
+                thread.start()
                 flash('开始重发！')
-        return render_template("pacp.html", netname=netname, needpacp=needpacp, form=form)
-    return redirect(url_for('index'))
+        return render_template("pcap.html", netname=netname, needpcap=needpcap, form=form, stop=stop, form1=form1)
+    return redirect(url_for('index'))   
 
 @app.errorhandler(400)
 def bad_request(e):
@@ -84,4 +106,4 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 if __name__ ==  "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
