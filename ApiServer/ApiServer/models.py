aee39883f5ac4251746b29a5b0ac6d84b04ea38e
99kies <1290017556@qq.com>
2020-04-22 22:19:55 +0800

    Update is_super

--- ApiServer/ApiServer/models.py
+++ ApiServer/ApiServer/models.py
@@ -11,7 +11,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20))
     password_hash = db.Column(db.String(128))
-    is_super = db.Column(Boolean)
+    is_super = db.Column(db.Boolean)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
