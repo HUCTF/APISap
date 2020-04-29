730648edec715ce52e70cde99f6c6582a65af8e1
99kies <1290017556@qq.com>
2020-04-29 18:58:28 +0800

    Add Manager.py

---
+++ ScanServer/manager.py
@@ -0,0 +1,81 @@
+from ScanServer import create_app
+from ScanServer.extensions import db
+from ScanServer.models import User
+
+from flask_script import Manager, Server, Shell
+import click
+app = create_app()
+
+banner = r"""
+"""
+
+manager = Manager(app)
+
+
+def make_shell_context():
+    return {
+        "app": app,
+    }
+
+
+manager.add_command("runserver", Server(host="0.0.0.0", port=5000, use_debugger=True))
+manager.add_command("shell", Shell(banner=banner, make_context=make_shell_context))
+
+
+@manager.command
+def reset_db():
+    """Initialize the database."""
+        
+    click.confirm('This operation will delete the database, do you want to continue?', abort=True)
+    db.drop_all()
+    click.echo('Drop tables.')
+    db.create_all()
+    click.echo('Initialized database.')
+    admin = User(
+        username='admin',
+        email='admin@admin.com',
+    )
+    admin.set_password('admin')
+    db.session.add(admin)
+    db.session.commit()
+    click.echo('Success Add Admin Count.')
+
+@manager.command
+def init_db():
+    db.create_all()
+    click.echo('Initialized database.')
+    admin = User(
+        username='admin',
+        email='admin@admin.com',
+    )
+    admin.set_password('admin')
+    db.session.add(admin)
+    db.session.commit()
+    click.echo('Success Add Admin Count.')
+
+@manager.command
+def set_user(username, email, password):
+    """Building Bluelog, just for you."""
+
+    click.echo('Initializing the database...')
+    db.create_all()
+
+    user = User.query.first()
+    if user is not None:
+        click.echo('The useristrator already exists, updating...')
+        user.username = username
+        user.email = email
+        user.set_password(password)
+    else:
+        click.echo('Creating the temporary useristrator account...')
+        user = User(
+            username=username,
+            email=email,
+        )
+        user.set_password(password)
+        db.session.add(user)
+        db.session.commit()
+
+if __name__ == "__main__":
+    manager.run()
+
