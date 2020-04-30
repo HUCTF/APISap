f050a99f2ef232d79ff77753fe6a121b2a4f6ce3
99kies <1290017556@qq.com>
2020-05-01 03:16:31 +0800

    update

--- ScanServer/ScanServer/forms.py
+++ ScanServer/ScanServer/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField, IntegerField, TextAreaField, PasswordField, BooleanField
-from wtforms.validators import DataRequired, Length, Required, Email
+from wtforms.validators import DataRequired, Length, Required, Email, Optional, URL
 
 class NameForm(FlaskForm):
     # text = StringField( "网卡名",validators= [DataRequired()])
@@ -26,10 +26,35 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember me')
     submit = SubmitField('Log in')
 
-
 class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(1, 20)])
     email = StringField('Email', validators=[DataRequired('请输入邮箱'), Email('邮箱格式不正确')])
     password = PasswordField('Password', validators=[DataRequired(), Length(1, 128)])
     password1 = PasswordField('Re Password', validators=[DataRequired(), Length(1, 128)])
     submit = SubmitField('Register')
+
+
+class InitForm(FlaskForm):
+    website = StringField('网页', validators=[Optional(), URL(), Length(0, 255)])
+    submit = SubmitField( "提交")
+
+class WorkForm(FlaskForm):
+    spider = SubmitField("开启扫描敏感数据")
+    repeter = SubmitField("重发数据包")
+
+class AimUserForm1(FlaskForm):
+    username_or_email = StringField('Username / Email / Phone', validators=[DataRequired('请输入账号')])
+    password = PasswordField('Password', validators=[DataRequired('请输入密码'), Length(1, 128)])
+
+class AimUserForm2(FlaskForm):
+    username_or_email = StringField('Username / Email / Phone', validators=[DataRequired('请输入账号')])
+    password = PasswordField('Password', validators=[DataRequired('请输入密码'), Length(1, 128)])
+
+
+
+class CookieForm1(FlaskForm):
+    usercookie = StringField('TestCookie1', validators=[DataRequired('请输入有效Cookie')])
+
+class CookieForm1(FlaskForm):
+    usercookie = StringField('TestCookie2', validators=[DataRequired('请输入有效Cookie')])
+
