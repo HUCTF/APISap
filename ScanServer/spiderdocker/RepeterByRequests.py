5a74aa4f7754ec94d043666cb0d8e8f5cea5447a
99kies <1290017556@qq.com>
2020-05-02 12:15:26 +0800

    Add Spiderdocker

---
+++ ScanServer/spiderdocker/RepeterByRequests.py
@@ -0,0 +1,108 @@
+#coding:utf-8
+import sys
+import requests
+import time
+import re
+from scapy.utils import PcapReader
+##resend the package in package.txt, and get the return from server.
+# host=''
+cookienum = 2
+cookieB = {'Cookie':'456789test'}
+cookieC = {'Cookie':'159786test'}
+# fd=open(sys.path[0]+'/packpost.txt', errors='ignore')
+fd=open(sys.path[0]+'/package.txt', errors='ignore')
+def getmidstring(html, start_str, end):
+    start = html.find(start_str)
+    if start >= 0:
+        start += len(start_str)
+        end = html.find(end, start)
+        if end >= 0:
+            return str(html[start:end].strip())
+def Read_package():
+    line=fd.readline()
+    if not line:
+        return False
+    while ('GET' not in line) and ('POST' not in line):
+        line=fd.readline()
+    if 'GET' in line:
+        msd='GET'
+        host = getmidstring(line, 'GET ', ' HTTP/')
+        print("===========GET==========")
+        print(host)
+        # print("=========================")
+        requests_get(line, host)
+    else:
+       msd='POST'
+       host=getmidstring(line,'POST ',' HTTP/')
+       print("===========post==========")
+       print(host)
+       # print("=========================")
+       requests_post(line, host)
+    return True
+
+       #############################
+       #####  ���Ż�������      #####
+       #############################
+
+
+def requests_get(line, host):
+    line = line[1:]
+    headers={}
+    cookie = {}
+    headers.clear()
+    cookie.clear()
+    while("'" not in line):
+        line = fd.readline()
+        head = line.split(": ")
+        if "\n" not in head[0] and head[0] != '':
+            headers[head[0]]=head[1][:-1]
+    host='http://'+str(headers['Host'])+str(host)
+    if 'Cookie' in headers.keys():
+        cookie.setdefault('Cookie',headers['Cookie'])
+        del headers['Cookie']
+        print(cookie)
+    print(headers)
+    for i in range(1,n+1):
+    # resp=requests.get(url=host,headers=headers,cookies=cookie).text
+    # print(resp)
+
+
+
+def requests_post(line, host):
+    line = line[1:]
+    headers={}
+    data={}
+    cookie ={}
+    headers.clear()
+    data.clear()
+    cookie.clear()
+    # time.sleep(1)
+    print(line)
+    line = fd.readline()
+    while("'" not in line and ":" in line):
+        head = line.split(": ")
+        if "\n" not in head[0] and head[0] != '' :
+            headers[head[0]]=head[1][:-1]
+        line = fd.readline()
+    line = fd.readline()
+    if re.match(".+'", line):
+        line = line[:-2]
+        datas= line.split("&&")
+        for l in datas:
+            l.split('=')
+            data[l[0]]=l[2]
+    if 'Cookie' in headers.keys():
+        cookie.setdefault('Cookie',headers['Cookie'])
+        del headers['Cookie']
+        print(cookie)
+    print(headers)
+    print(data)
+    # host = 'http://' + str(headers['Host']) + str(host)
+    # resp=requests.post(url=host,headers=headers,data=data).text
+    # print(resp)
+
+if __name__ == "__main__":
+    while(1):
+        flag = Read_package()
+        if not flag:
+            break
