5b984cbcd753161f23eb71b0563b6a7a05c9bca8
99kies <1290017556@qq.com>
2020-05-06 18:41:16 +0800

    run python in docker

--- ScanServer/spiderdocker/RUNThread.py
+++ ScanServer/spiderdocker/RUNThread.py
@@ -3,19 +3,31 @@ from threading import Thread
 from .scanapi import RQRUN
 import os
 
-def RUNThread(website, choice):
-    thread = Thread(target=NICRUN, args=[netname, needpcap, str(current_user.username)])
-    # 使用多线程
-    thread.start()
-    thread = Thread(target=RQRUN)
-    # 使用多线程
-    thread.start()
-    
-    thread = Thread(target=send_async_email, args=[app, message])
-    thread.start()
 
-WEBSITE=os.environ.get('WEBSITE')
-USERNAME=os.environ.get('USERNAME')
-PASSWD=os.environ.get('PASSWD')
-COOKIE=os.environ.get('COOKIE')
+
+class spider():
+    def __init__(self):
+        self.WEBSITE=os.environ.get('WEBSITE')
+        self.RUNWAY=os.environ.get('RUNWAY')
+        self.USERNAME=os.environ.get('USERNAME')
+
+        if RUNWAY == 'cookie':
+            self.COOKIE1 = os.environ.get('COOKIE1')
+            self.COOKIE2 = os.environ.get('COOKIE2')
+        elif RUNWAY == 'userid':
+            self.USERID1 = os.environ.get('USERID1')
+            self.PASSWD1 = os.environ.get('PASSWD1')
+            self.USERID2 = os.environ.get('USERID2')
+            self.PASSWD2 = os.environ.get('PASSWD2')
+
+    def RUNThread(self):
+        needpcap = 1000
+        thread = Thread(target=NICRUN, args=['eth0', needpcap, self.USERNAME])
+        # 使用多线程
+        thread.start()
+
+        thread = Thread(target=RQRUN)
+        # 使用多线程
+        thread.start()
+    
 
