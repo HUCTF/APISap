ca6b85dda5aea6afd5d7e398a88286dc34f7430d
99kies <1290017556@qq.com>
2020-05-10 20:13:24 +0800

    update

--- ScanServer/spiderdocker/RUNThread.py
+++ ScanServer/spiderdocker/RUNThread.py
@@ -1,8 +1,8 @@
-from .NIC_package_get import NICRUN
+from NIC_package_get import NICRUN
 from threading import Thread
-from .scanapi import RQRUN
+#from .scanapi import RQRUN
 import os
-
+import requests
 
 
 class spider():
@@ -11,23 +11,51 @@ class spider():
         self.RUNWAY=os.environ.get('RUNWAY')
         self.USERNAME=os.environ.get('USERNAME')
 
-        if RUNWAY == 'cookie':
+        if self.RUNWAY == 'cookie':
             self.COOKIE1 = os.environ.get('COOKIE1')
             self.COOKIE2 = os.environ.get('COOKIE2')
-        elif RUNWAY == 'userid':
+        elif self.RUNWAY == 'userid':
             self.USERID1 = os.environ.get('USERID1')
             self.PASSWD1 = os.environ.get('PASSWD1')
             self.USERID2 = os.environ.get('USERID2')
             self.PASSWD2 = os.environ.get('PASSWD2')
 
-    def RUNThread(self):
+    def RUNThread_Cookie(self):
         needpcap = 1000
         thread = Thread(target=NICRUN, args=['eth0', needpcap, self.USERNAME])
         # 使用多线程
         thread.start()
 
-        thread = Thread(target=RQRUN)
+#        thread = Thread(target=RQRUN)
         # 使用多线程
+#        thread.start()
+        print('cookie')
+        print('cookie')
+        print('cookie')
+        print('cookie')
+        print(self.COOKIE1, self.COOKIE2)
+        for i in range(10):
+            r = requests.get('https://baidu.com').text
+            print(r)
+
+
+        
+    def RUNThread_Userid(self):
+        needpcap = 1000
+        thread = Thread(target=NICRUN, args=['eth0', needpcap, self.USERNAME])
         thread.start()
+ 
+
+        print('userid')
+        print('userid')
+        print('userid')
+        print('userid')
+        print(self.USERID1, self.PASSWD1, self.USERID2, self.PASSWD2)
     
+
+a = spider()
+if a.RUNWAY=='cookie':
+    a.RUNThread_Cookie()
+else:
+    a.RUNThread_Userid()
 
