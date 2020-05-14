d34b9fab07d38287ad03447c7a84aa44da91e277
99kies <1290017556@qq.com>
2020-05-14 10:39:55 +0800

    update RepeterByRequests

--- ScanServer/ScanServer/scanapi/v5/RepeterByRequests.py
+++ ScanServer/ScanServer/scanapi/v5/RepeterByRequests.py
@@ -9,99 +9,101 @@ from scapy.utils import PcapReader
 cookienum = 2
 cookieB = {'Cookie':'456789test'}
 cookieC = {'Cookie':'159786test'}
-# fd=open(sys.path[0]+'/packpost.txt', errors='ignore')
-fd=open(sys.path[0]+'/package.txt', errors='ignore')
-def getmidstring(html, start_str, end):
-    start = html.find(start_str)
-    if start >= 0:
-        start += len(start_str)
-        end = html.find(end, start)
-        if end >= 0:
-            return str(html[start:end].strip())
-def Read_package():
-    line=fd.readline()
-    if not line:
-        return False
-    while ('GET' not in line) and ('POST' not in line):
-        line=fd.readline()
-    if 'GET' in line:
-        msd='GET'
-        host = getmidstring(line, 'GET ', ' HTTP/')
-        print("===========GET==========")
-        print(host)
-        # print("=========================")
-        requests_get(line, host)
-    else:
-       msd='POST'
-       host=getmidstring(line,'POST ',' HTTP/')
-       print("===========post==========")
-       print(host)
-       # print("=========================")
-       requests_post(line, host)
-    return True
 
-       #############################
-       #####  ���Ż�������      #####
-       #############################
+class RepeterByRequests:
+
+    def __init__(self, username):
+        self.username = username
+        self.filename = '/opt/2020-Works-ApiSecurity/ScanServer/userfile_center/{0}/{0}_spider/{0}.txt'.format(self.username)
+        self.fd=open(self.filename, errors='ignore')
+        while(1):
+            flag = self.Read_package()
+            if not flag:
+                break
 
 
-def requests_get(line, host):
-    line = line[1:]
-    headers={}
-    cookie = {}
-    headers.clear()
-    cookie.clear()
-    while("'" not in line):
-        line = fd.readline()
-        head = line.split(": ")
-        if "\n" not in head[0] and head[0] != '':
-            headers[head[0]]=head[1][:-1]
-    host='http://'+str(headers['Host'])+str(host)
-    if 'Cookie' in headers.keys():
-        cookie.setdefault('Cookie',headers['Cookie'])
-        del headers['Cookie']
-        print(cookie)
-    print(headers)
-#    for i in range(1,n+1):
-    # resp=requests.get(url=host,headers=headers,cookies=cookie).text
-    # print(resp)
 
-def requests_post(line, host):
-    line = line[1:]
-    headers={}
-    data={}
-    cookie ={}
-    headers.clear()
-    data.clear()
-    cookie.clear()
+    def getmidstring(self, html, start_str, end):
+        start = html.find(start_str)
+        if start >= 0:
+            start += len(start_str)
+            end = html.find(end, start)
+            if end >= 0:
+                return str(html[start:end].strip())
+
+    def Read_package(self):
+        line=self.fd.readline()
+        if not line:
+            return False
+        while ('GET' not in line) and ('POST' not in line):
+            line=self.fd.readline()
+        if 'GET' in line:
+            msd='GET'
+            host = self.getmidstring(line, 'GET ', ' HTTP/')
+            print("===========GET==========")
+            print(host)
+            self.requests_get(line, host)
+        else:
+           msd='POST'
+           host=self.getmidstring(line,'POST ',' HTTP/')
+           print("===========post==========")
+           print(host)
+           self.requests_post(line, host)
+        return True
+
+
+    def requests_get(self, line, host):
+        line = line[1:]
+        headers={}
+        cookie = {}
+        headers.clear()
+        cookie.clear()
+        while("'" not in line):
+            line = self.fd.readline()
+            head = line.split(": ")
+            if "\n" not in head[0] and head[0] != '':
+                headers[head[0]]=head[1][:-1]
+        host='http://'+str(headers['Host'])+str(host)
+        if 'Cookie' in headers.keys():
+            cookie.setdefault('Cookie',headers['Cookie'])
+            del headers['Cookie']
+            print(cookie)
+        print(headers)
+
+    def requests_post(self, line, host):
+        line = line[1:]
+        headers={}
+        data={}
+        cookie ={}
+        headers.clear()
+        data.clear()
+        cookie.clear()
     # time.sleep(1)
-    print(line)
-    line = fd.readline()
-    while("'" not in line and ":" in line):
-        head = line.split(": ")
-        if "\n" not in head[0] and head[0] != '' :
-            headers[head[0]]=head[1][:-1]
-        line = fd.readline()
-    line = fd.readline()
-    if re.match(".+'", line):
-        line = line[:-2]
-        datas= line.split("&&")
-        for l in datas:
-            l.split('=')
-            data[l[0]]=l[2]
-    if 'Cookie' in headers.keys():
-        cookie.setdefault('Cookie',headers['Cookie'])
-        del headers['Cookie']
-        print(cookie)
-    print(headers)
-    print(data)
-    # host = 'http://' + str(headers['Host']) + str(host)
-    # resp=requests.post(url=host,headers=headers,data=data).text
-    # print(resp)
+        print(line)
+        line = self.fd.readline()
+        while("'" not in line and ":" in line):
+            head = line.split(": ")
+            if "\n" not in head[0] and head[0] != '' :
+                headers[head[0]]=head[1][:-1]
+            line = self.fd.readline()
+        line = self.fd.readline()
+        if re.match(".+'", line):
+            line = line[:-2]
+            datas= line.split("&&")
+            for l in datas:
+                l.split('=')
+                data[l[0]]=l[2]
+        if 'Cookie' in headers.keys():
+            cookie.setdefault('Cookie',headers['Cookie'])
+            del headers['Cookie']
+            print(cookie)
+        print(headers)
+        print(data)
 
 
-
-
+def RUNRepeter(filename):
+    while(1):
+        flag = RepeterByRequests(filename)
 
 if __name__ == "__main__":
     while(1):
