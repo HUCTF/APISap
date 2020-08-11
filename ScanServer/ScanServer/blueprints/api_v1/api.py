6ea6e8b40422371dcbada3eea59da44b8d6d205b
99kies <1290017556@qq.com>
2020-08-11 23:08:40 +0800

    update

--- ScanServer/ScanServer/blueprints/api_v1/api.py
+++ ScanServer/ScanServer/blueprints/api_v1/api.py
@@ -3,6 +3,8 @@ from ScanServer.utils import get_txt_file, api_abort
 from flask import render_template, request, flash, redirect, url_for,  jsonify, Blueprint
 from flask_login import login_required, current_user, login_user, logout_user
 
+import os
+
 api_v1 = Blueprint('api_v1', __name__)
 
 
@@ -15,34 +17,54 @@ def package_msg():
     if filename:
         s = get_txt_file(filename)
         return jsonify({
-                    'code':'200',
-                    'result':str(s),
-                    'done':'done',
-               })
+            'code':'200',
+            'result':str(s),
+            'done':'done',
+        })
     else:
         return jsonify({
-                    'code':'400',
-                    'result':'please input filename!',
-               })
+            'code':'400',
+            'result':'please input filename!',
+        })
 
 @api_v1.route('/deployment/delete', methods=["GET"])
 @login_required
 def delete_container():
     delete_deployment(current_user.username)
-    return jsonify({"result": "delete success", "code":"200"})
+    return jsonify({
+        "result": "Delete Successful", 
+        "code":"200"
+    })
+
+@api_v1.route("/deployment/reset", methods=["GET"])
+@login_required
+def reset_container():
+    status = os.system("kubectl -n scanserver delete po --all")
+    if status:
+        return jsonify({
+            "result":"Reset Successful",
+            "code":"200"
+        })
+    else:
+        return jsonify({
+            "result":"Reset Failed",
+            "code":"404"
+        })
 
 @api_v1.route('deployment/pod/status', methods=["GET"])
 @login_required
 def pod_status():
-    status = os.system('kubectl -n scanserver get po | grep kies')
+    status = os.system('kubectl -n scanserver get po | grep {}'.format(current_user.username))
     if status == 0:
         return jsonify({
-            "result": "RUNNING",
+            "podname": "scanserver-{}".format(current_user.username),
+            "status":"Running",
             "code": "200"
         })
     else:
         return jsonify({
             "result": "None",
+            "status":"Died",
             "code": "404"
         })
 
